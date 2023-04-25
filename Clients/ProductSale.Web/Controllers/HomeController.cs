94b76de6fd3504c3c8255090355c0be3d09aebfc
nijatabdull <nijat.abdullayev@aist.group>
2023-04-25 08:47:06 +0400

    home ctrl was developed

--- Clients/ProductSale.Web/Controllers/HomeController.cs
+++ Clients/ProductSale.Web/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ProductSale.Web.Exceptions;
 using ProductSale.Web.Models;
+using ProductSale.Web.Services.Abstractions;
 using System.Diagnostics;
 
 namespace ProductSale.Web.Controllers
@@ -8,24 +11,34 @@ namespace ProductSale.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ICatalogService _catalogService;
+
+        public HomeController(ILogger<HomeController> logger, ICatalogService catalogService)
         {
             _logger = logger;
+            _catalogService = catalogService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _catalogService.GetAllCoursesAsync());
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Detail(string id)
         {
-            return View();
+            return View(await _catalogService.GetCourseByIdAsync(id));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            var errorFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            if (errorFeature != null && errorFeature.Error is UnauthorizedException)
+            {
+                return RedirectToAction(nameof(AuthController.SignOut), "Auth");
+            }
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
